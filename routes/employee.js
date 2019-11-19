16bfa311080991bae4f237dc63261c863dfa8423
akhi <dakhilesh95@gmail.com>
2019-11-20 01:01:57 +0530

    container

--- routes/employee.js
+++ routes/employee.js
@@ -5,7 +5,8 @@ const connection= mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"manager",
-    database:"mean"
+    database:"mean",
+    port:"9099"
 });
 connection.connect();
 app.get("/",function(req,res){
